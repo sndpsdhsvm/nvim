5cc52c7b02577d25ac9cebec3f5fec72c6523d0f
sndpsdhsvm <sandeep.sadhasivam@gmail.com>
2023-11-08 11:35:58 +0000

    treesitter r/w access buh

--- lua/sndp/plugins/treesitter.lua
+++ lua/sndp/plugins/treesitter.lua
@@ -1,4 +1,6 @@
+vim.opt.runtimepath:append("/home/sndp/.config/.nvim-treesitter/parsers")
 require'nvim-treesitter.configs'.setup {
+  parser_install_dir = "/home/sndp/.config/.nvim-treesitter/parsers",
   -- A list of parser names, or "all"
   ensure_installed = { "html", "javascript", "scss", "typescript", "bash" },
 
