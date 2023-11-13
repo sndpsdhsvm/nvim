470b9033582e7a376ce0c3506e83f0f0235d5b1b
sndpsdhsvm <sandeep.sadhasivam@gmail.com>
2023-11-13 18:30:36 +0000

    latest merged

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+local Plugin = { "williamboman/mason.nvim" }
+
+Plugin.lazy = false
+
+-- See :help mason-settings
+Plugin.opts = {
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+}
+
+return Plugin
